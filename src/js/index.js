c2801cfcbc5da66d29c531b81fbf8068ed711d15
HollosJ <hollosj99@gmail.com>
2020-12-13 17:59:24 +0000

    Removed unnecessary code

--- src/js/index.js
+++ src/js/index.js
@@ -38,17 +38,14 @@ nav_btns.forEach(btn => {
 const stories = [
     {
         img: "img/storiesimg1.jpg",
-        name: "John Doe",
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium maxime, recusandae explicabo voluptate similique unde dolore illum earum eveniet consequuntur quis reprehenderit suscipit voluptatibus inventore eaque blanditiis sit tempora exercitationem, repudiandae magni provident maiores. Eligendi sapiente saepe id non eos exercitationem amet harum, dolores reprehenderit suscipit! Ab mollitia beatae amet!"
     },
     {
         img: "img/storiesimg2.jpg",
-        name: "Jane Doe",
         text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor temporibus qui vitae odio aspernatur! Cupiditate dolores, ullam tenetur harum quia tempora totam quidem porro, a nisi consequuntur accusantium commodi blanditiis minus consectetur dolor! Corporis facilis mollitia voluptate quam dolorum quaerat?"
     },
     {
         img: "img/storiesimg3.jpg",
-        name: "Joe Bloggs",
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quaerat earum velit ex laudantium placeat. Incidunt omnis dolorem tempore, praesentium, ea suscipit repellat officia ab, eum tempora ducimus! Culpa quos et fugit similique fugiat, cupiditate praesentium, labore assumenda facilis odio recusandae minima neque alias mollitia obcaecati reprehenderit consequatur nemo aperiam blanditiis."
     }
 ]
