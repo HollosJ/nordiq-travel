88e2b42ecd686297492c24031073407041ba0b50
HollosJ <hollosj99@gmail.com>
2021-11-11 00:15:42 +0000

    Cursor changes

--- src/js/index.js
+++ src/js/index.js
@@ -10,9 +10,9 @@ nav_toggle.addEventListener("click", () => {
 const cursor = document.getElementById("cursor");
 const hoverables = document.querySelectorAll(".hoverable");
 
-document.addEventListener("mousemove", (e) => {
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
+document.addEventListener("mousemove", e => {
+    cursor.style.left = (e.clientX - 20) + "px";
+    cursor.style.top = (e.clientY - 20) + "px";
 });
 
 //Add active class to cursor when hovering over chosen elements
@@ -60,7 +60,6 @@ let time = 10000;
 changeImg = () => {
     storyImg.src = stories[i].img;
     storyText.innerText = `"${stories[i].text}"`;
-    console.log(dots[i])
 
     dots.forEach(dot => {
         dot.classList.remove("active")
